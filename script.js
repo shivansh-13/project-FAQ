c0ec7f0e079b6e80d3d51a7950139b669bac9f3b
shivansh-13 <80212477+shivansh-13@users.noreply.github.com>
2021-03-26 00:04:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+var question1=document.getElementById("que1");
+var dropdown1=document.getElementById("more1");
+var quel1=document.getElementById("quel1");
+
+
+dropdown1.onclick = function(){
+
+    if(question1.className == "open"){
+        
+        question1.className="";
+        quel1.className="";
+
+
+    }else{
+
+        question1.className = "open";
+        quel1.className="bold";
+    }
+};
+var question2=document.getElementById("que2");
+var dropdown2=document.getElementById("more2");
+var quel2=document.getElementById("quel2");
+
+dropdown2.onclick = function(){
+
+    if(question2.className == "open"){
+        
+        question2.className="";
+        quel2.className="";
+
+
+    }else{
+
+        question2.className = "open";
+        quel2.className="bold";
+    }
+};
+var question3=document.getElementById("que3");
+var dropdown3=document.getElementById("more3");
+var quel3=document.getElementById("quel3");
+
+dropdown3.onclick = function(){
+
+    if(question3.className == "open"){
+        
+        question3.className="";
+        quel3.className="";
+
+
+    }else{
+
+        question3.className = "open";
+        quel3.className="bold";
+    }
+};
+var question4=document.getElementById("que4");
+var dropdown4=document.getElementById("more4");
+var quel4=document.getElementById("quel4");
+
+dropdown4.onclick = function(){
+
+    if(question4.className == "open"){
+        
+        question4.className="";
+        quel4.className="";
+
+
+    }else{
+
+        question4.className = "open";
+        quel4.className="bold";
+    }
+};
+var question5=document.getElementById("que5");
+var dropdown5=document.getElementById("more5");
+var quel5=document.getElementById("quel5");
+
+dropdown5.onclick = function(){
+
+    if(question5.className == "open"){
+        
+        question5.className="";
+        quel5.className="";
+
+
+    }else{
+
+        question5.className = "open";
+        quel5.className="bold";
+    }
+};
+
+
+
+
